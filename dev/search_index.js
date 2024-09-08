5c672f563c780f468144ec067fb4e8a24187bc09
Documenter.jl <documenter@juliadocs.github.io>
2024-09-08 17:15:54 +0000

    build based on 6e40e51

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PlotMultiLevelData","category":"page"},{"location":"#PlotMultiLevelData","page":"Home","title":"PlotMultiLevelData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PlotMultiLevelData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PlotMultiLevelData]","category":"page"}]
+}
